1a0aacdfd2c7fd02fc73757db327965a44dafaf8
hushenggui <15131270956@163.com>
2020-04-07 12:54:30 +0800

    项目初始化

---
+++ boot-queue/src/main/java/tuacy/microservice/framweork/bootqueue/BootQueueApplication.java
@@ -0,0 +1,13 @@
+package tuacy.microservice.framweork.bootqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootQueueApplication.class, args);
+    }
+
+}
---
+++ boot-queue/src/test/java/tuacy/microservice/framweork/bootqueue/BootQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package tuacy.microservice.framweork.bootqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
