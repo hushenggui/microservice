666c35b29381f670eb8bc86141b465063a31017d
hushenggui <hushenggui@cngongbao.com>
2020-07-17 14:52:12 +0800

    项目初始化

--- boot-queue/src/main/java/tuacy/microservice/framweork/bootqueue/BootQueueApplication.java
+++ boot-queue/src/main/java/tuacy/microservice/framweork/bootqueue/BootQueueApplication.java
@@ -1,9 +1,15 @@
 package tuacy.microservice.framweork.bootqueue;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableRabbit
+@EnableEurekaClient
+@Slf4j
 public class BootQueueApplication {
 
     public static void main(String[] args) {
