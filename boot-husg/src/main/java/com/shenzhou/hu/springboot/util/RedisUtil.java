983fbe9e25b53dd9143b7b98bcdd97f5fb126549
hushenggui <hushenggui@cngongbao.com>
2020-09-07 10:20:54 +0800

    公共模块整合

---
+++ boot-husg/src/main/java/com/shenzhou/hu/springboot/util/RedisUtil.java
@@ -0,0 +1,165 @@
+package com.shenzhou.hu.springboot.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: cngongbao_test_tool
+ * @description: redis工具类
+ * @author: hushenggui
+ * @create: 2020-06-28 18:14
+ **/
+@Component
+public class RedisUtil {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    /**
+     * 普通缓存获取
+     * @param key 键
+     * @return 值
+     */
+    public Object get(String key){
+        return key==null?null:redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 普通缓存放入
+     * @param key 键
+     * @param value 值
+     * @return true成功 false失败
+     */
+    public boolean set(String key,Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 普通缓存放入并设置时间
+     * @param key 键
+     * @param value 值
+     * @param time 时间(秒) time要大于0 如果time小于等于0 将设置无限期
+     * @return true成功 false 失败
+     */
+    public boolean set(String key,Object value,long time){
+        try {
+            if(time>0){
+                redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+            }else{
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * @Description: 将list数据放入缓存
+     * @Param: [key, list]
+     * @return:
+     * @Author: hushenggui
+     * @Date: 2020/6/2
+     */
+    public boolean pList(String key, List list){
+        try {
+            if(!CollectionUtils.isEmpty(list)){
+                list.stream().forEach(object ->{
+                    redisTemplate.opsForList().rightPush(key,object);
+                });
+            }
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * @Description: 将list数据放入缓存,并设置过期时间
+     * @Param: [key, list]
+     * @return:
+     * @Author: hushenggui
+     * @Date: 2020/6/2
+     */
+    public boolean pList(String key, List list,long time){
+        try {
+            if(!CollectionUtils.isEmpty(list)){
+                list.stream().forEach(object ->{
+                    redisTemplate.opsForList().rightPush(key,object);
+                });
+                expire(key,time);
+            }
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * 指定缓存失效时间
+     * @param key 键
+     * @param time 时间(秒)
+     * @return
+     */
+    public boolean expire(String key,long time){
+        try {
+            if(time>0){
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * @Description: 模糊匹配删除key
+     * @Param: [pattern]
+     * @return:
+     * @Author: hushenggui
+     * @Date: 2020/6/4
+     */
+    public void deleteKey(String pattern){
+        Set<String> keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
+    /**
+     * 获取hashKey对应的所有键值
+     * @param key 键
+     * @return 对应的多个键值
+     */
+    public Map<Object,Object> hmget(String key){
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    /**
+     * HashSet
+     * @param key 键
+     * @param map 对应多个键值
+     * @return true 成功 false 失败
+     */
+    public boolean hmset(String key, Map<String,Object> map){
+        try {
+            redisTemplate.opsForHash().putAll(key, map);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
